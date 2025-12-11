d44aeeb73a96daf7d5fde8e3b85c720e85a10655
Amadeus Jackson <nottherealmozart@gmail.com>
2025-12-11 18:25:16 -0500

    Added full Siri, support and accessibility features

---
+++ Daily Joy/Intents/AppIntents.swift
@@ -0,0 +1,8 @@
+//
+//  AppIntents.swift
+//  Daily Joy
+//
+//  Created by Amadeus Jackson on 12/10/25.
+//
+
+import Foundation
---
+++ Tabs/Momonts/ReducedMotionHelper.swift
@@ -0,0 +1,8 @@
+//
+//  ReducedMotionHelper.swift
+//  Daily Joy
+//
+//  Created by Amadeus Jackson on 12/11/25.
+//
+
+import Foundation
