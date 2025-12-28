38e9d16810a2e1b5a282c433b36f599e380d800f
Amadeus Jackson <nottherealmozart@gmail.com>
2025-12-28 17:14:32 -0500

    added notifications and a pause after adding a moment

---
+++ Daily Joy/Custom Views/NotificationManager.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationManager.swift
+//  Daily Joy
+//
+//  Created by Amadeus Jackson on 12/28/25.
+//
+
+import Foundation
---
+++ Daily Joy/Models/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  Daily Joy
+//
+//  Created by Amadeus Jackson on 12/28/25.
+//
+
+import Foundation
