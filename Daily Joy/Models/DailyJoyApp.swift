8e76e9ef8f64e16f5e948ec13b75ab005b1a6c8f
NotTheRealMozart <nottherealmozart@gmail.com>
2026-02-09 13:26:51 -0500

    added smater notafacions

---
+++ Daily Joy/Models/DailyJoyApp.swift
@@ -0,0 +1,22 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Amadeus Jackson on 11/14/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DailyJoyApp: App {
+    private let dataContainer = DataContainer()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(dataContainer)
+                .modelContainer(dataContainer.modelContainer)
+        }
+    }
+}
